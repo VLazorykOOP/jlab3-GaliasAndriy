0c60fedd9088395a905c2f5c52ce5ddfc4096f59
beistroff <galiasandrii@gmail.com>
2023-03-20 10:11:25 +0200

    litle change in EngineTask.java

--- src/engines/EngineTask.java
+++ src/engines/EngineTask.java
@@ -1,13 +1,5 @@
-//Побудувати ієрархію класів відповідно до варіанта завдання.
-//        Згідно завдання вибрати суперклас (базовий клас) та підкласи (похідні класи). В
-//        класах задати поля, які характерні для кожного класу. Для всіх класів
-//        розробити метод Show(), який виводить дані про об’єкт класу. Розробити
-//        програму, яка вводить інформацію про об’єкти заданих сутностей згідно
-//        варіанту в масив типу суперкласу та друкує введений масив (з використанням
-//        методу Show()).
-
-// 12.Двигун, двигун внутрішнього згоряння, дизель, реактивний двигун.
 import java.util.Scanner;
+
 public class EngineTask {
     public static void main(String[] args) {
         System.out.println(" Lab 3");
@@ -58,14 +50,5 @@ public class EngineTask {
             System.out.println(engines[i].show());
         }
         engines[0].getType();
-//        BaseEngine[] engines = new BaseEngine[3];
-//        engines[0] = new InternalCombustionEngine("Gasoline", "Gasoline", 4, 75.0, 265.0, 1.3);
-//        engines[1] = new InternalCombustionEngine("Gasoline", "Gasoline", 4, 75.0, 265.0, 1.3);
-//        engines[2] = new InternalCombustionEngine("Gasoline", "Gasoline", 4, 75.0, 265.0, 1.3);
-//        for (BaseEngine engine : engines) {
-//            System.out.println("\n Engine: ");
-//            engine.show();
-//            System.out.println("");
-//        }
     }
 }
